8f7c51bb2d97b0da9c3d92393a318f07a596d143
rezaulete <rezaulapece@gmail.com>
2020-12-08 19:44:55 +0600

    teacher commit

--- src/main/java/school/controller/AdminController/AdminTeacherController.java
+++ src/main/java/school/controller/AdminController/AdminTeacherController.java
@@ -19,26 +19,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import school.model.Student;
 import school.model.Teacher;
 import school.model.TeacherPhoto;
-import school.model.teacher;
-import school.model.teacherDetails;
-import school.model.teacherPhoto;
 import school.model.enumvalue.Gender;
 import school.model.enumvalue.Groups;
 import school.model.enumvalue.Isactive;
 import school.model.enumvalue.Regularity;
 import school.model.enumvalue.Schclass;
 import school.model.enumvalue.Version;
-import school.repository.SectionRepository;
-import school.repository.StsessionRepository;
 import school.repository.TeacherPhotoRepository;
-import school.repository.teacherDetailsRepository;
-import school.repository.teacherPhotoRepository;
-import school.repository.teacherRepository;
 import school.repository.TeacherRepository;
 import school.services.FilePhotoService;
-import school.services.teacherService;
+
 
 
 /**
@@ -47,7 +40,7 @@ import school.services.teacherService;
 */
 
 @Controller
-@RequestMapping("/dashboards/teacher")
+@RequestMapping("/dashboards/teachers")
 public class AdminTeacherController {
 
 	@Autowired
@@ -57,6 +50,8 @@ public class AdminTeacherController {
 	@Autowired
 	FilePhotoService filePhotoService;
 
+	private static String UPLOAD_FOLDER = "src//main//resources//static//images//StudentPhoto//";
+	
 	// Open Class for Teacher table
 	@RequestMapping(value = { "/", "/index" })
 	public String home(Model model) {
@@ -113,7 +108,6 @@ public class AdminTeacherController {
 	//Delete teacher file by id
 	@RequestMapping("/delete-teacher")
 	public String delete(Model model, @RequestParam Long id, RedirectAttributes redirectAttributes) {
-		Teacher teacher = teacherRepository.findById(id);
 
 			  teacherRepository.delete(id); 
 
@@ -129,12 +123,8 @@ public class AdminTeacherController {
 	public String edit(Model model, @RequestParam Long id) {
 		Teacher teacher = teacherRepository.findById(id);
 
-		model.addAttribute("teacher", teacherRepository.findById(id));
-		model.addAttribute("Version", Version.values());
+		model.addAttribute("teacher", teacher);
 		model.addAttribute("Gender", Gender.values());
-		model.addAttribute("Regularity", Regularity.values());
-		model.addAttribute("Groups", Groups.values());
-		model.addAttribute("Isactive", Isactive.values());
 		return "dashboards/teachers/edit";
 	}
 
@@ -150,7 +140,6 @@ public class AdminTeacherController {
 			model.addAttribute("teacher", teacher);
 			model.addAttribute("recent", teacherRepository.findByOrderByIdDesc());
 			model.addAttribute("Gender", Gender.values());
-			model.addAttribute("Isactive", Isactive.values());
 			return "dashboards/teachers/edit";
 		}
 		teacherRepository.save(teacher);
@@ -194,7 +183,7 @@ public class AdminTeacherController {
         	model.addAttribute("teacher", teacher);
         	model.addAttribute("teacherPhoto",teacherPhoto);
         
-         return "dashboards/teachers/insertPhoto";
+         return "dashboards/students/insertPhoto";
   
                 }
         
@@ -203,195 +192,200 @@ public class AdminTeacherController {
  		        model.addAttribute("errormessage", "Something wrong...");
  		        model.addAttribute("teacherPhoto",teacherPhoto); 
  		        model.addAttribute("teacher", teacher);
- 		        return "dashboards/teachers/insertPhoto";
+ 		        return "dashboards/students/insertPhoto";
        }	 
        
  		try {
- 			filePhotoService.UploadFile(file);
+ 			// read and write the file to the selected location-
+ 			byte[] bytes = file.getBytes();
+ 			Path path = Paths.get(UPLOAD_FOLDER +teacherPhoto.getPhototitle()+file.getOriginalFilename());
+ 			Files.write(path, bytes);
+ 			teacherPhoto.setPicFile(teacherPhoto.getPhototitle()+file.getOriginalFilename());
+ 			System.out.println("File size:"+file.getSize());
 
  		} catch (IOException e) {
  			e.printStackTrace();
  		}
  				    	
- 		        teacherPhotoRepository.save(teacherPhoto);
+ 		teacherPhotoRepository.save(teacherPhoto);
  		        redirectAttributes.addAttribute("stid", teacher.getId());
                 return "redirect:/dashboards/teachers/view-teacher/{stid}";
     }
     
     
     
-    
-    // Delete teacher Photo by id 
- 	@RequestMapping("/delete-teacherphoto") 
-    public String teacherphotodelete(Model model
-    		,@RequestParam Long id, RedirectAttributes redirectAttributes) {
- 		
- 		TeacherPhoto teacherPhoto=teacherPhotoRepository.findById(id); 
- 		Long stid=teacherPhoto.getTeacher().getId();
- 		Teacher teacher= teacherRepository.findById(stid);
- 		redirectAttributes.addAttribute("stid", teacher.getId());
- 		
- 		try { 
-            Path path=Paths.get(teacherPhoto.getPicFile());
-            Files.delete(path);
-            System.out.println("Image Deleted !!!"); 
- 		}catch(Exception e)
-        {
-            System.out.println("Failed to Delete image !!");            
-        }		
- 		     teacherPhotoRepository.delete(id);  		
- 		    		    
- 		    return "redirect:/dashboards/teachers/view-teacher/{stid}";
-    }
- 	
- 	
- 	
-    // Open edit teacher Photo page
- 	@RequestMapping("/edit-teacherphoto/{stid}") 
- 	public String edits(Model model, @PathVariable("stid") Long stid) {        
-        model.addAttribute("teacherphoto",teacherPhotoRepository.findById(stid));        
-        return "dashboards/teachers/editPhoto";
- 	}
- 	
- 	
- 	
- 	
-    // Update teacher new Photo 
- 	@PostMapping("/update-teacherphoto") 
-    public String teacherphotoUpdate(Model model,@ModelAttribute teacherPhoto teacherPhoto
-    		, BindingResult bindingResult
-    		,@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes
-    		) {	
- 		
- 		int size=(int) file.getSize(); 
- 		Long stid=teacherPhoto.getteacher().getId();
-    	teacher teacher= teacherRepository.findById(stid);
- 		if (file.isEmpty()) {
- 			teacherPhoto.setPicFile(teacherPhoto.getPicFile());			
- 	      	}
- 		else  if(size>1000000){
-         	model.addAttribute("errormessage", "Input file is more than 1MB");
-         	model.addAttribute("message", "File is to large. please thy file below 1MB ");
- 			model.addAttribute("teacherPhoto",teacherPhoto); 
- 		    return "dashboards/teachers/editPhoto";
-   
-                 }
- 		 else {
- 				try { 
- 		            Path path=Paths.get(UPLOAD_FOLDER+teacherPhoto.getPicFile());
- 		            Files.delete(path);
- 		            System.out.println("Image Deleted !!!");        
- 				}catch(Exception e)
- 		        {
- 		            System.out.println("Failed to Delete image !!");       
- 		           
- 		        }				
- 				try {
- 					// read and write the file to the selected location-
- 					byte[] bytes = file.getBytes();
- 					Path path = Paths.get(UPLOAD_FOLDER +teacherPhoto.getPhototitle()+file.getOriginalFilename());
- 					Files.write(path, bytes);
- 					teacherPhoto.setPicFile(teacherPhoto.getPhototitle()+file.getOriginalFilename());					
-
- 				}  catch (IOException e)  {    
- 		             e.printStackTrace();
- 				         }		
- 			    }
-
-           //If there are some error...	
-         if (bindingResult.hasErrors()) {	
- 		        model.addAttribute("errormessage", "Something wrong...");		 
- 		        model.addAttribute("teacherPhoto",teacherPhoto);  
- 		        return "dashboards/teachers/editPhoto";
-        }	         		    	
-                  teacherPhotoRepository.save(teacherPhoto);
-                  redirectAttributes.addAttribute("stid", teacher.getId());
-                  return "redirect:/dashboards/teachers/view-teacher/{stid}";
-    }
-
- 	
-     //////teacher DETAILS PART//////
-	//Insert teacher Details page
- 	
-    @RequestMapping("/insertdetails/{stid}")
-    public String teacherdetailinsert(Model model, @PathVariable("stid") Long stid) {
-    	
-    	model.addAttribute("teacher", teacherRepository.findById(stid));
-    	model.addAttribute("teacherdetail", new teacherDetails());
-        return "dashboards/teachers/insertdetail";
-    }
-    
-    
-	// Save teacher Details 
-	@PostMapping("/save-teacherdetail")
-	public String savedetails(Model model, @Valid @ModelAttribute("teacherDetails") teacherDetails teacherDetails, BindingResult bindingResult,
-			RedirectAttributes redirectAttributes) {
-		
- 		Long stid=teacherDetails.getteacher().getId();
- 		teacher teacher= teacherRepository.findById(stid);
-		// If there are some error...
-		if (bindingResult.hasErrors()) {
-
-			model.addAttribute("errorteacher", "Something wrong...");
-			model.addAttribute("teacherDetails", teacherDetails);
-			return "dashboards/teachers/insert";
-		}
-
-		teacherDetailsRepository.save(teacherDetails);
-		redirectAttributes.addAttribute("stid", teacher.getId());
-        return "redirect:/dashboards/teachers/view-teacher/{stid}";
-	}
-
-    // Delete teacher Photo by id 
- 	@RequestMapping("/delete-teacherdetails") 
-    public String teacherdetailsdelete(Model model
-    		,@RequestParam Long id, RedirectAttributes redirectAttributes) {
- 		
- 		teacherDetails teacherDetails=teacherDetailsRepository.findById(id); 
- 		Long stid=teacherDetails.getteacher().getId();
- 		teacher teacher= teacherRepository.findById(stid);
- 		redirectAttributes.addAttribute("stid", teacher.getId());
- 		
- 		try {
- 			teacherDetailsRepository.delete(id); 
-
-		} catch (Exception e) {
-			model.addAttribute("errormessage", "Can't delete this data.");
-			model.addAttribute("error", e.getMessage());
-			return "redirect:/dashboards/teachers/view-teacher/{stid}";
-		}    		    
- 		    return "redirect:/dashboards/teachers/view-teacher/{stid}";
-    }
-	
-	
-    // Open edit teacher Details page
- 	@RequestMapping("/edit-teacherdetails/{stid}") 
- 	public String editsdetails(Model model, @PathVariable("stid") Long stid) {        
- 		model.addAttribute("teacherdetail", teacherDetailsRepository.findById(stid));       
-        return "dashboards/teachers/editdetail";
- 	}
- 	
- 	
-    // Update teacher new Details 
-  	@PostMapping("/update-teacherdetails") 
-     public String teacherDetailsUpdate(Model model,@ModelAttribute teacherDetails teacherDetails
-     		, BindingResult bindingResult, RedirectAttributes redirectAttributes
-     		) {	
- 
-  		Long stid=teacherDetails.getteacher().getId();
-     	teacher teacher= teacherRepository.findById(stid);
-     	
-     	if (bindingResult.hasErrors()) {
-     		model.addAttribute("errormessage", "Something wrong...");		 
-		        model.addAttribute("teacherDetails",teacherDetails);  
-		        return "dashboards/teachers/editdetail";
-     		
-		}
-     		
-     	           teacherDetailsRepository.save(teacherDetails);
-                   redirectAttributes.addAttribute("stid", teacher.getId());
-                   return "redirect:/dashboards/teachers/view-teacher/{stid}";
-     }
+//    
+//    // Delete teacher Photo by id 
+// 	@RequestMapping("/delete-teacherphoto") 
+//    public String teacherphotodelete(Model model
+//    		,@RequestParam Long id, RedirectAttributes redirectAttributes) {
+// 		
+// 		TeacherPhoto teacherPhoto=teacherPhotoRepository.findById(id); 
+// 		Long stid=teacherPhoto.getTeacher().getId();
+// 		Teacher teacher= teacherRepository.findById(stid);
+// 		redirectAttributes.addAttribute("stid", teacher.getId());
+// 		
+// 		try { 
+//            Path path=Paths.get(teacherPhoto.getPicFile());
+//            Files.delete(path);
+//            System.out.println("Image Deleted !!!"); 
+// 		}catch(Exception e)
+//        {
+//            System.out.println("Failed to Delete image !!");            
+//        }		
+// 		     teacherPhotoRepository.delete(id);  		
+// 		    		    
+// 		    return "redirect:/dashboards/teachers/view-teacher/{stid}";
+//    }
+// 	
+// 	
+// 	
+//    // Open edit teacher Photo page
+// 	@RequestMapping("/edit-teacherphoto/{stid}") 
+// 	public String edits(Model model, @PathVariable("stid") Long stid) {        
+//        model.addAttribute("teacherphoto",teacherPhotoRepository.findById(stid));        
+//        return "dashboards/teachers/editPhoto";
+// 	}
+// 	
+// 	
+// 	
+// 	
+//    // Update teacher new Photo 
+// 	@PostMapping("/update-teacherphoto") 
+//    public String teacherphotoUpdate(Model model,@ModelAttribute teacherPhoto teacherPhoto
+//    		, BindingResult bindingResult
+//    		,@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes
+//    		) {	
+// 		
+// 		int size=(int) file.getSize(); 
+// 		Long stid=teacherPhoto.getteacher().getId();
+//    	teacher teacher= teacherRepository.findById(stid);
+// 		if (file.isEmpty()) {
+// 			teacherPhoto.setPicFile(teacherPhoto.getPicFile());			
+// 	      	}
+// 		else  if(size>1000000){
+//         	model.addAttribute("errormessage", "Input file is more than 1MB");
+//         	model.addAttribute("message", "File is to large. please thy file below 1MB ");
+// 			model.addAttribute("teacherPhoto",teacherPhoto); 
+// 		    return "dashboards/teachers/editPhoto";
+//   
+//                 }
+// 		 else {
+// 				try { 
+// 		            Path path=Paths.get(UPLOAD_FOLDER+teacherPhoto.getPicFile());
+// 		            Files.delete(path);
+// 		            System.out.println("Image Deleted !!!");        
+// 				}catch(Exception e)
+// 		        {
+// 		            System.out.println("Failed to Delete image !!");       
+// 		           
+// 		        }				
+// 				try {
+// 					// read and write the file to the selected location-
+// 					byte[] bytes = file.getBytes();
+// 					Path path = Paths.get(UPLOAD_FOLDER +teacherPhoto.getPhototitle()+file.getOriginalFilename());
+// 					Files.write(path, bytes);
+// 					teacherPhoto.setPicFile(teacherPhoto.getPhototitle()+file.getOriginalFilename());					
+//
+// 				}  catch (IOException e)  {    
+// 		             e.printStackTrace();
+// 				         }		
+// 			    }
+//
+//           //If there are some error...	
+//         if (bindingResult.hasErrors()) {	
+// 		        model.addAttribute("errormessage", "Something wrong...");		 
+// 		        model.addAttribute("teacherPhoto",teacherPhoto);  
+// 		        return "dashboards/teachers/editPhoto";
+//        }	         		    	
+//                  teacherPhotoRepository.save(teacherPhoto);
+//                  redirectAttributes.addAttribute("stid", teacher.getId());
+//                  return "redirect:/dashboards/teachers/view-teacher/{stid}";
+//    }
+//
+// 	
+//     //////teacher DETAILS PART//////
+//	//Insert teacher Details page
+// 	
+//    @RequestMapping("/insertdetails/{stid}")
+//    public String teacherdetailinsert(Model model, @PathVariable("stid") Long stid) {
+//    	
+//    	model.addAttribute("teacher", teacherRepository.findById(stid));
+//    	model.addAttribute("teacherdetail", new teacherDetails());
+//        return "dashboards/teachers/insertdetail";
+//    }
+//    
+//    
+//	// Save teacher Details 
+//	@PostMapping("/save-teacherdetail")
+//	public String savedetails(Model model, @Valid @ModelAttribute("teacherDetails") teacherDetails teacherDetails, BindingResult bindingResult,
+//			RedirectAttributes redirectAttributes) {
+//		
+// 		Long stid=teacherDetails.getteacher().getId();
+// 		teacher teacher= teacherRepository.findById(stid);
+//		// If there are some error...
+//		if (bindingResult.hasErrors()) {
+//
+//			model.addAttribute("errorteacher", "Something wrong...");
+//			model.addAttribute("teacherDetails", teacherDetails);
+//			return "dashboards/teachers/insert";
+//		}
+//
+//		teacherDetailsRepository.save(teacherDetails);
+//		redirectAttributes.addAttribute("stid", teacher.getId());
+//        return "redirect:/dashboards/teachers/view-teacher/{stid}";
+//	}
+//
+//    // Delete teacher Photo by id 
+// 	@RequestMapping("/delete-teacherdetails") 
+//    public String teacherdetailsdelete(Model model
+//    		,@RequestParam Long id, RedirectAttributes redirectAttributes) {
+// 		
+// 		teacherDetails teacherDetails=teacherDetailsRepository.findById(id); 
+// 		Long stid=teacherDetails.getteacher().getId();
+// 		teacher teacher= teacherRepository.findById(stid);
+// 		redirectAttributes.addAttribute("stid", teacher.getId());
+// 		
+// 		try {
+// 			teacherDetailsRepository.delete(id); 
+//
+//		} catch (Exception e) {
+//			model.addAttribute("errormessage", "Can't delete this data.");
+//			model.addAttribute("error", e.getMessage());
+//			return "redirect:/dashboards/teachers/view-teacher/{stid}";
+//		}    		    
+// 		    return "redirect:/dashboards/teachers/view-teacher/{stid}";
+//    }
+//	
+//	
+//    // Open edit teacher Details page
+// 	@RequestMapping("/edit-teacherdetails/{stid}") 
+// 	public String editsdetails(Model model, @PathVariable("stid") Long stid) {        
+// 		model.addAttribute("teacherdetail", teacherDetailsRepository.findById(stid));       
+//        return "dashboards/teachers/editdetail";
+// 	}
+// 	
+// 	
+//    // Update teacher new Details 
+//  	@PostMapping("/update-teacherdetails") 
+//     public String teacherDetailsUpdate(Model model,@ModelAttribute TeacherDetails teacherDetails
+//     		, BindingResult bindingResult, RedirectAttributes redirectAttributes
+//     		) {	
+// 
+//  		Long stid=teacherDetails.getteacher().getId();
+//     	Teacher teacher= teacherRepository.findById(stid);
+//     	
+//     	if (bindingResult.hasErrors()) {
+//     		model.addAttribute("errormessage", "Something wrong...");		 
+//		        model.addAttribute("teacherDetails",teacherDetails);  
+//		        return "dashboards/teachers/editdetail";
+//     		
+//		}
+//     		
+////     	           teacherDetailsRepository.save(teacherDetails);
+//                   redirectAttributes.addAttribute("stid", teacher.getId());
+//                   return "redirect:/dashboards/teachers/view-teacher/{stid}";
+//     }
 
  	
 }
--- src/main/java/school/repository/TeacherRepository.java
+++ src/main/java/school/repository/TeacherRepository.java
@@ -5,12 +5,10 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 import school.model.Teacher;
-import school.model.enumvalue.Schclass;
 public interface TeacherRepository extends CrudRepository<Teacher, Long>{
 
 	public Teacher findById(Long id);
 	public List<Teacher> findByOrderByIdDesc();
 	public Teacher findTopByOrderByIdDesc();
-	public List<Teacher> findBySchclass(Schclass schclass);
 
 }
